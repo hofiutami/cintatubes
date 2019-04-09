fd71f611155825a4c28639c0d47a405ad6c6de74
syafeq <49190603+threonine@users.noreply.github.com>
2019-04-09 18:46:00 +0700

    Add files via upload

---
+++ header.h
@@ -0,0 +1,10 @@
+#IFNDEF HEADER_H
+#DEFINE HEADER_H
+
+typedef struct prak_s {
+	int ada; // 0 kalo belum ditentukan ada praktikum, 1 kalo ada praktikum
+	int kode;
+	int asist;
+} prak_t;
+
+#ENDIF
